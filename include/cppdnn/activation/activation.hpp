3cfd2edfbec222c7bf18efca680aef08c2a70a11
wong73 <120403030+wong73@users.noreply.github.com>
2020-11-21 17:53:50 +0000

    Version bump to 0.1.6

--- include/cppdnn/activation/activation.hpp
+++ include/cppdnn/activation/activation.hpp
@@ -1,4 +1,5 @@
 #include <cppdnn/activation/activation_layer.hpp>
 #include <cppdnn/activation/relu_layer.hpp>
 #include <cppdnn/activation/sigmoid_layer.hpp>
+#include <cppdnn/activation/softmax_layer.hpp>
 #incl
