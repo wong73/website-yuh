838638ff8f11aef7f69d274d1d28f2b5f20ac67f
wong73 <120403030+wong73@users.noreply.github.com>
2020-11-30 07:38:04 +0000

    update travis build distro

--- include/cppdnn/activation/activation_layer.hpp
+++ include/cppdnn/activation/activation_layer.hpp
@@ -1,4 +1,7 @@
 #ifndef CPPDNN_HEADER_ACTIVATION_ACTIVATION_LAYER_HPP
 #define CPPDNN_HEADER_ACTIVATION_ACTIVATION_LAYER_HPP
 
-#include <cppdnn/layer.hpp
+#include <cppdnn/layer.hpp>
+#include <cppdnn/object.hpp>
+
+namespace c
