3023b1ed7d1130d3a70f8df48c77d0aed6dc90d0
wong73 <120403030+wong73@users.noreply.github.com>
2021-01-09 12:04:21 +0000

    Updating the version

--- include/cppdnn/activation/activation_layer.hpp
+++ include/cppdnn/activation/activation_layer.hpp
@@ -21,4 +21,4 @@ namespace cppdnn
 		basic_activation_layer& operator=(const basic_activation_layer& layer) = delete;
 		basic_activation_layer& operator=(basic_activation_layer&& layer) = delete;
 		bool operator==(const basic_activation_layer& layer) = delete;
-		bool ope
+		bool operator!=(const basic_activation_layer& l
