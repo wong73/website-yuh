47faf31aad2c26e17e8e315e170ee8bd715f2f86
wong73 <120403030+wong73@users.noreply.github.com>
2021-01-25 22:42:22 +0000

    fix stupid error

--- include/cppdnn/activation/activation_layer.hpp
+++ include/cppdnn/activation/activation_layer.hpp
@@ -27,4 +27,7 @@ namespace cppdnn
 		virtual void forward(const basic_object_ptr<Ty_>& input, basic_object_ptr<Ty_>& output) const override;
 		
 	protected:
-		virtual void function(const basic_object_ptr<Ty_>& input) con
+		virtual void function(const basic_object_ptr<Ty_>& input) const = 0;
+	};
+
+	using activation_layer 
