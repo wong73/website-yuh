825a8007054116417af19e94a94e95a16154561f
wong73 <120403030+wong73@users.noreply.github.com>
2021-03-09 11:41:39 +0000

    Typical ashok

--- include/cppdnn/activation/details/relu_layer.hpp
+++ include/cppdnn/activation/details/relu_layer.hpp
@@ -6,4 +6,6 @@
 namespace cppdnn
 {
 	template<typename Ty_>
-	void basic_relu_layer<Ty_>::function(const basic_object_p
+	void basic_relu_layer<Ty_>::function(const basic_object_ptr<Ty_>& input) const
+	{
+		input->a
