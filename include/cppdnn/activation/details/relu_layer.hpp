36dbc076b3db5dd2a3726846e905a9a39ee523a8
wong73 <120403030+wong73@users.noreply.github.com>
2021-03-10 12:42:43 +0000

    regmap: fix possible ZERO_SIZE_PTR pointer dereferencing error.

--- include/cppdnn/activation/details/relu_layer.hpp
+++ include/cppdnn/activation/details/relu_layer.hpp
@@ -8,4 +8,4 @@ namespace cppdnn
 	template<typename Ty_>
 	void basic_relu_layer<Ty_>::function(const basic_object_ptr<Ty_>& input) const
 	{
-		input->a
+		input->apply([](const basic_object_ptr<Ty_>&
