917e253aca91fe64148ae90991ae07727d71c5ab
wong73 <120403030+wong73@users.noreply.github.com>
2021-06-05 06:44:30 +0000

    Updated map

--- include/cppdnn/activation/softmax_layer.hpp
+++ include/cppdnn/activation/softmax_layer.hpp
@@ -1,4 +1,8 @@
 #ifndef CPPDNN_HEADER_ACTIVATION_SOFTMAX_LAYER_HPP
 #define CPPDNN_HEADER_ACTIVATION_SOFTMAX_LAYER_HPP
 
-#include <cppdnn/activation/activation_l
+#include <cppdnn/activation/activation_layer.hpp>
+
+namespace cppdnn
+{
+	templ
