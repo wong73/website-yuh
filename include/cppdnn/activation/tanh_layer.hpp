bc0a12fa1a596498155c35482d9f302d17b85a80
wong73 <120403030+wong73@users.noreply.github.com>
2021-08-05 11:48:08 +0000

    another small change

--- include/cppdnn/activation/tanh_layer.hpp
+++ include/cppdnn/activation/tanh_layer.hpp
@@ -1,4 +1,8 @@
 #ifndef CPPDNN_HEADER_ACTIVATION_TANH_LAYER_HPP
 #define CPPDNN_HEADER_ACTIVATION_TANH_LAYER_HPP
 
-#include <cppdnn/activation/activatio
+#include <cppdnn/activation/activation_layer.hpp>
+
+namespace cppdnn
+{
+	template
