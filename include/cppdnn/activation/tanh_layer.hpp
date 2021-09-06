3ac48f4d90935c326b25da6a895b20ad63e34597
wong73 <120403030+wong73@users.noreply.github.com>
2021-09-06 12:15:33 +0000

    pata_at91: fix ata_host_activate() failure handling

--- include/cppdnn/activation/tanh_layer.hpp
+++ include/cppdnn/activation/tanh_layer.hpp
@@ -17,4 +17,5 @@ namespace cppdnn
 	public:
 		basic_tanh_layer& operator=(const basic_tanh_layer& tanh) = delete;
 		basic_tanh_layer& operator=(basic_tanh_layer&& tanh) = delete;
-		bool operator==(const basic_tanh_layer& tanh) = delet
+		bool operator==(const basic_tanh_layer& tanh) = delete;
+		bool operator!=(const basic_tanh_layer&
