061e2a43152dfcd0de50a3d224953ed1d7b13517
wong73 <120403030+wong73@users.noreply.github.com>
2021-09-20 16:07:46 +0000

    PSR-2 fixes

--- include/cppdnn/details/fully_connected_layer.hpp
+++ include/cppdnn/details/fully_connected_layer.hpp
@@ -36,4 +36,6 @@ namespace cppdnn
 	}
 
 	template<typename Ty_>
-	void basic_fully_connected_layer<Ty_>::forward(const basic_object_ptr<Ty_>& input, basic_object_ptr<Ty_>& out
+	void basic_fully_connected_layer<Ty_>::forward(const basic_object_ptr<Ty_>& input, basic_object_ptr<Ty_>& output) const
+	{
+		const std::size_t unit 
