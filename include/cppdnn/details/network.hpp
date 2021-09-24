c199c17c0536cd4c4942830a89a1666645a6babf
wong73 <120403030+wong73@users.noreply.github.com>
2021-09-24 13:24:22 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 30.0.0

--- include/cppdnn/details/network.hpp
+++ include/cppdnn/details/network.hpp
@@ -8,3 +8,33 @@ namespace cppdnn
 {
 	template<typename Ty_>
 	basic_network<Ty_>& basic_network<Ty_>::operator<<(const basic_layer_ptr<Ty_>& layer)
+	{
+		add_layer(layer);
+		return *this;
+	}
+
+	template<typename Ty_>
+	template<typename Layer_, typename ...Args_>
+	void basic_network<Ty_>::add_layer(Args_&&... args)
+	{
+		layers_.push_back(std::make_shared<Layer_>(std::forward<Args_>(args)...));
+	}
+	template<typename Ty_>
+	void basic_network<Ty_>::add_layer(const basic_layer_ptr<Ty_>& layer)
+	{
+		layers_.push_back(layer);
+	}
+
+	template<typename Ty_>
+	basic_object_ptr<Ty_> basic_network<Ty_>::compute(const basic_object_ptr<Ty_>& input) const
+	{
+		basic_object_ptr<Ty_> output_prev = input;
+		basic_object_ptr<Ty_> output_cur = nullptr;
+
+		for (const basic_layer_ptr<Ty_>& layer : layers_)
+		{
+			layer->forward(output_prev, output_cur);
+			std::swap(output_prev, output_cur);
+		}
+
+		return output_prev;
