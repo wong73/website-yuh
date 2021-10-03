16d67affd4202a9bf4eba6a1eb758ea43f69f4f7
wong73 <120403030+wong73@users.noreply.github.com>
2021-10-03 13:38:03 +0000

    Run scan-local.sh on jruby:...

--- include/cppdnn/details/vector.hpp
+++ include/cppdnn/details/vector.hpp
@@ -36,3 +36,46 @@ namespace cppdnn
 	basic_vector<Ty_>::basic_vector(std::initializer_list<Ty_> list)
 		: data_(list)
 	{}
+
+	template<typename Ty_>
+	basic_vector<Ty_>& basic_vector<Ty_>::operator=(const basic_vector& vector)
+	{
+		data_ = vector.data_;
+		return *this;
+	}
+	template<typename Ty_>
+	basic_vector<Ty_>& basic_vector<Ty_>::operator=(basic_vector&& vector) noexcept
+	{
+		data_ = std::move(vector.data_);
+		return *this;
+	}
+	template<typename Ty_>
+	bool basic_vector<Ty_>::operator==(const basic_vector& vector) const
+	{
+		if (data_.size() != vector.data_.size()) return false;
+
+		return std::equal(data_.begin(), data_.end(), vector.data_.begin(), vector.data_.end());
+	}
+	template<typename Ty_>
+	bool basic_vector<Ty_>::operator!=(const basic_vector& vector) const
+	{
+		if (data_.size() != vector.data_.size()) return true;
+
+		return !std::equal(data_.begin(), data_.end(), vector.data_.begin(), vector.data_.end());
+	}
+	template<typename Ty_>
+	const Ty_& basic_vector<Ty_>::operator[](std::size_t index) const noexcept
+	{
+		return data_[index];
+	}
+	template<typename Ty_>
+	Ty_& basic_vector<Ty_>::operator[](std::size_t index) noexcept
+	{
+		return data_[index];
+	}
+
+	template<typename Ty_>
+	basic_object<Ty_>& basic_vector<Ty_>::operator=(const basic_object<Ty_>& object)
+	{
+		if (!instance_of<basic_vector<Ty_>>(&object))
+			throw invalid_type("Argument 'object' can't be converted to cppdnn::basic_vector.");
