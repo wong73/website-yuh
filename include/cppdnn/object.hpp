96c065f1da87c6c50147bfd23c70c3107fb01d1c
wong73 <120403030+wong73@users.noreply.github.com>
2021-11-10 10:54:26 +0000

    update books

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -34,4 +34,4 @@ namespace cppdnn
 		virtual std::shared_ptr<basic_object<Ty_>> operator+(const basic_object& object) const = 0;
 		virtual std::shared_ptr<basic_object<Ty_>> operator*(const basic_object& object) const = 0;
 		virtual basic_object& operator+=(const basic_object& object) = 0;
-		virtu
+		virtual basic_object& operator*=(cons
