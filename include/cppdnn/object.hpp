4a8e4a7a3a73c2138e8f51584800be65e2ea0c4e
wong73 <120403030+wong73@users.noreply.github.com>
2021-11-12 10:49:23 +0000

    Adding error checking

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -40,4 +40,5 @@ namespace cppdnn
 		virtual bool is_value() const noexcept;
 
 		virtual std::shared_ptr<basic_object<Ty_>> copy() const = 0;
-		virtual void for_each(const std::function<void(std::shared_ptr<basic_object<Ty_>>)>& fun
+		virtual void for_each(const std::function<void(std::shared_ptr<basic_object<Ty_>>)>& func) const = 0;
+		virtual void apply(const
