bcc8003cc57ec44556839822fb1491e82446c1e2
wong73 <120403030+wong73@users.noreply.github.com>
2021-11-15 12:49:41 +0000

    Remove commented code

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -55,4 +55,5 @@ namespace cppdnn
 		void print(std::ostream& stream, bool detail) const;
 		void println() const;
 		void println(bool detail) const;
-		void printl
+		void println(std::ostream& stream) const;
+		void println(s
