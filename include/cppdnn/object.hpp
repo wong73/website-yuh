8f82514eac112bfc922ffce7b498642cdc2ed367
wong73 <120403030+wong73@users.noreply.github.com>
2021-11-15 22:46:16 +0000

    Packaging.

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -62,4 +62,7 @@ namespace cppdnn
 	using object = basic_object<double>;
 	template<typename Ty_>
 	using basic_object_ptr = std::shared_ptr<basic_object<Ty_>>;
-	using object_ptr = basic_object
+	using object_ptr = basic_object_ptr<double>;
+
+	template<typename Ty_>
+	
