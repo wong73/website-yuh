9591620c17d7a3fc7cbb5263f236fe9b29c4000c
wong73 <120403030+wong73@users.noreply.github.com>
2021-11-19 21:36:36 +0000

    Delete new

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -84,4 +84,4 @@ namespace cppdnn
 		virtual ~basic_value() override = default;
 	
 	public:
-		basic_value& operator=(const basic_value& value) noe
+		basic_value& operator=(const basic_value& value) noexcept(std::is_nothrow_copy_assignable<Ty_>:
