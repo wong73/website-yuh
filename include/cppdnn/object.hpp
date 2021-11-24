ab502297c0803adcae2ddf88a62cbfbe0a7bcbe4
wong73 <120403030+wong73@users.noreply.github.com>
2021-11-24 09:48:17 +0000

    Fix leak

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -87,4 +87,6 @@ namespace cppdnn
 		basic_value& operator=(const basic_value& value) noexcept(std::is_nothrow_copy_assignable<Ty_>::value);
 		basic_value& operator=(basic_value&& value) noexcept(std::is_nothrow_move_assignable<Ty_>::value);
 		bool operator==(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() == std::declval<Ty_>()));
-		bool operator!=(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() != std::d
+		bool operator!=(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() != std::declval<Ty_>()));
+		
+		virtua
