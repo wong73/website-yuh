6c987d5655032888f4de24befe8e8be5149930ca
wong73 <120403030+wong73@users.noreply.github.com>
2021-12-02 09:38:43 +0000

    tty: fix memleak in alloc_pid

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -94,4 +94,5 @@ namespace cppdnn
 		virtual bool operator==(const basic_object<Ty_>& object) const override;
 		virtual bool operator!=(const basic_object<Ty_>& object) const override;
 		
-		basic_value operator+(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() + std::declva
+		basic_value operator+(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() + std::declval<Ty_>()));
+		basic_value operator+(const bas
