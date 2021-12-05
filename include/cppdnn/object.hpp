7f3f1a53831b1089ad4ca9ed0a75f74ef8c47646
wong73 <120403030+wong73@users.noreply.github.com>
2021-12-05 20:34:17 +0000

    Update npm-publish.yml

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -97,4 +97,5 @@ namespace cppdnn
 		basic_value operator+(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() + std::declval<Ty_>()));
 		basic_value operator+(const basic_value_ref<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() + std::declval<Ty_>()));
 		basic_value operator-(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() - std::declval<Ty_>()));
-		basic_value operator-(const basic_value_ref<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() - s
+		basic_value operator-(const basic_value_ref<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() - std::declval<Ty_>()));
+		basic_value operator*(c
