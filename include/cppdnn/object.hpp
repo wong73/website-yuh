440e387dc97bc402afbe193c936f5dda42d5924f
wong73 <120403030+wong73@users.noreply.github.com>
2021-12-08 11:09:24 +0000

    Fixed class name

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -98,4 +98,4 @@ namespace cppdnn
 		basic_value operator+(const basic_value_ref<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() + std::declval<Ty_>()));
 		basic_value operator-(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() - std::declval<Ty_>()));
 		basic_value operator-(const basic_value_ref<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() - std::declval<Ty_>()));
-		basic_value operator*(c
+		basic_value operator*(const basic_value& value) const noexce
