ac5e97c69eac1524e74fb70c0512d984ec951e8f
wong73 <120403030+wong73@users.noreply.github.com>
2021-12-11 16:28:46 +0000

    A bit of code cleanup

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -101,4 +101,4 @@ namespace cppdnn
 		basic_value operator*(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() * std::declval<Ty_>()));
 		basic_value operator*(const basic_value_ref<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() * std::declval<Ty_>()));
 		basic_value operator/(const basic_value& value) const noexcept(noexcept(std::declval<Ty_>() / std::declval<Ty_>()));
-		basic_
+		basic_value operator/(const basic_value_ref<Ty
