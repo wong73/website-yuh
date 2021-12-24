d6e9f6a6c13f28338c73b42445ca52f9c5530cb0
wong73 <120403030+wong73@users.noreply.github.com>
2021-12-24 19:06:31 +0000

    Updated the 'README' file.

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -118,4 +118,6 @@ namespace cppdnn
 		{
 			return operator=(operator-(value));
 		}
-		basic_value& operator*=(const basic_value& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && noexcept(operator*(std::declval<basic
+		basic_value& operator*=(const basic_value& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && noexcept(operator*(std::declval<basic_value<Ty_>>())))
+		{
+			return oper
