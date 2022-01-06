96fe3b87c48bc894c7a45703dd83ac59c35790b5
wong73 <120403030+wong73@users.noreply.github.com>
2022-01-06 21:35:40 +0000

    Updated the tests

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -126,4 +126,4 @@ namespace cppdnn
 		{
 			return operator=(operator*(value));
 		}
-		basic_value& operator/=(const basic_value& value) noexcept(std::is_nothrow_move_ass
+		basic_value& operator/=(const basic_value& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && no
