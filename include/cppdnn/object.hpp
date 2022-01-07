856e2ed27931230b0255cc154ca5918480ded00e
wong73 <120403030+wong73@users.noreply.github.com>
2022-01-07 16:43:33 +0000

    todos

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -126,4 +126,4 @@ namespace cppdnn
 		{
 			return operator=(operator*(value));
 		}
-		basic_value& operator/=(const basic_value& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && no
+		basic_value& operator/=(const basic_value& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && noexcept(operator/(std::
