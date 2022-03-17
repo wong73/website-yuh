6527f4ae16181c941e49c253de07d08bccaac8d6
wong73 <120403030+wong73@users.noreply.github.com>
2022-03-17 20:42:25 +0000

    demo1

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -167,4 +167,5 @@ namespace cppdnn
 		basic_value_ref() noexcept = default;
 		basic_value_ref(Ty_& value) noexcept;
 		basic_value_ref(basic_value<Ty_>& value) noexcept;
-		basic_value_ref(const basic_value_ref& valu
+		basic_value_ref(const basic_value_ref& value) noexcept;
+		virtual ~basic_value_ref() ove
