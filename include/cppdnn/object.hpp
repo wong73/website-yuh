3d97cec25f8d02196bb224e3fdcade68ca81d7fe
wong73 <120403030+wong73@users.noreply.github.com>
2022-03-28 17:28:16 +0000

    Pontoon: Update Belarusian (be) localization of Firefox Screenshots

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -177,4 +177,5 @@ namespace cppdnn
 		bool operator!=(const basic_value_ref& value) const noexcept;
 
 		virtual basic_object<Ty_>& operator=(const basic_object<Ty_>& object) override;
-		virtual basic_object<Ty_>& operator=(basic_object<Ty_>&& obje
+		virtual basic_object<Ty_>& operator=(basic_object<Ty_>&& object) override;
+		virtual bool oper
