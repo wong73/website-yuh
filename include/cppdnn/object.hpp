183a1e0e29da7bd54a6ba9628ce49612455fec39
wong73 <120403030+wong73@users.noreply.github.com>
2022-03-31 17:49:03 +0000

    mm: Add notifier framework for showing memory

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -179,4 +179,4 @@ namespace cppdnn
 		virtual basic_object<Ty_>& operator=(const basic_object<Ty_>& object) override;
 		virtual basic_object<Ty_>& operator=(basic_object<Ty_>&& object) override;
 		virtual bool operator==(const basic_object<Ty_>& object) const override;
-		virtual bo
+		virtual bool operator!=(const basic_object<Ty_>& 
