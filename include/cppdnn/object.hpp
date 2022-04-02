0837ced5dff25b60192f1f6538d4e964ffd20c44
wong73 <120403030+wong73@users.noreply.github.com>
2022-04-02 09:53:50 +0000

    merged from origin

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -181,4 +181,5 @@ namespace cppdnn
 		virtual bool operator==(const basic_object<Ty_>& object) const override;
 		virtual bool operator!=(const basic_object<Ty_>& object) const override;
 
-		basic_value<Ty_> operator+(const basic_value_ref& value) const noexcept(noexcept(std::declval<Ty_>() + std::
+		basic_value<Ty_> operator+(const basic_value_ref& value) const noexcept(noexcept(std::declval<Ty_>() + std::declval<Ty_>()));
+		basic_value<Ty_> operato
