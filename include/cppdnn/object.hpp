5409c807ca958a5c04bfc34420aca24111a514a1
wong73 <120403030+wong73@users.noreply.github.com>
2022-04-07 10:39:18 +0000

    django.po: Update German translations

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -182,4 +182,5 @@ namespace cppdnn
 		virtual bool operator!=(const basic_object<Ty_>& object) const override;
 
 		basic_value<Ty_> operator+(const basic_value_ref& value) const noexcept(noexcept(std::declval<Ty_>() + std::declval<Ty_>()));
-		basic_value<Ty_> operator+(const basic_value<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() + st
+		basic_value<Ty_> operator+(const basic_value<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() + std::declval<Ty_>()));
+		basic_value<Ty_>
