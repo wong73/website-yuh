e114ad28b47146f2b270acbd818d893bba499bc8
wong73 <120403030+wong73@users.noreply.github.com>
2022-04-22 05:46:40 +0000

    Fixing layout

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -185,4 +185,4 @@ namespace cppdnn
 		basic_value<Ty_> operator+(const basic_value<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() + std::declval<Ty_>()));
 		basic_value<Ty_> operator-(const basic_value_ref& value) const noexcept(noexcept(std::declval<Ty_>() - std::declval<Ty_>()));
 		basic_value<Ty_> operator-(const basic_value<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() - std::declval<Ty_>()));
-		basic_value<Ty_> operator*(const basic_value_ref& value) const noexce
+		basic_value<Ty_> operator*(const basic_value_ref& value) const noexcept(noexcept(std::declval<Ty_>() * s
