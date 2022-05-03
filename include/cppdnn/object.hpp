9d201ad220e074cb814079815d06b70bde3d3f81
wong73 <120403030+wong73@users.noreply.github.com>
2022-05-03 09:07:50 +0000

    Remove submodules

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -187,4 +187,4 @@ namespace cppdnn
 		basic_value<Ty_> operator-(const basic_value<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() - std::declval<Ty_>()));
 		basic_value<Ty_> operator*(const basic_value_ref& value) const noexcept(noexcept(std::declval<Ty_>() * std::declval<Ty_>()));
 		basic_value<Ty_> operator*(const basic_value<Ty_>& value) const noexcept(noexcept(std::declval<Ty_>() * std::declval<Ty_>()));
-		basic_value<Ty_> operator/(const basic_value_ref& value) const noexcep
+		basic_value<Ty_> operator/(const basic_value_ref& value) const noexcept(noexcept(std::declval<Ty_>() / std::declval<T
