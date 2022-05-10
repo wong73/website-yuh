5aacef15998ddb102d070436dedf446d74565d4e
wong73 <120403030+wong73@users.noreply.github.com>
2022-05-10 13:24:19 +0000

    iwlwifi: fix TX power antenna access

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -193,4 +193,6 @@ namespace cppdnn
 		{
 			return operator=(operator+(value));
 		}
-		basic_value_ref& operator+=(const basic_value<Ty_>& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && noexcept(operator+(std::declval<basic_value<T
+		basic_value_ref& operator+=(const basic_value<Ty_>& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && noexcept(operator+(std::declval<basic_value<Ty_>>())))
+		{
+			return operator=(op
