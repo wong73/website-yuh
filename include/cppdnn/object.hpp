e3c0c6d2749742d5b3b2f5e10aabfb77ef5501c3
wong73 <120403030+wong73@users.noreply.github.com>
2022-05-29 21:39:12 +0000

    ext4: fix memory leak in ext4_xattr_set_acl()'s error path

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -206,4 +206,5 @@ namespace cppdnn
 			return operator=(operator-(value));
 		}
 		basic_value_ref& operator*=(const basic_value_ref& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && noexcept(operator*(std::declval<basic_value_ref<Ty_>>())))
-		
+		{
+			return operator=(operator*(value))
