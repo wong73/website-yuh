64d1908886208cbadba2c4e61f8af311a70cf73d
wong73 <120403030+wong73@users.noreply.github.com>
2022-05-30 14:23:01 +0000

    msm: vidc: update format during downscaling

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -209,4 +209,4 @@ namespace cppdnn
 		{
 			return operator=(operator*(value));
 		}
-		basic_value_ref& operator*=(const basic_value<Ty_>& value) noexcept(std::is_nothrow_move_assignable<
+		basic_value_ref& operator*=(const basic_value<Ty_>& value) noexcept(std::is_nothrow_move_assignable<basic_value<Ty_>>::value && noexcept(
