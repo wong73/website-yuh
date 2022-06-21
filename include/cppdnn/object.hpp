5cd666cc75cf21b35a4f4defe1329855e20b8395
wong73 <120403030+wong73@users.noreply.github.com>
2022-06-21 09:23:06 +0000

    add blogs

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -217,4 +217,4 @@ namespace cppdnn
 		{
 			return operator=(operator/(value));
 		}
-		basic_value_ref& operator/=(const basic_value<Ty_>& 
+		basic_value_ref& operator/=(const basic_value<Ty_>& value) noexcept(std::is_nothrow_move
