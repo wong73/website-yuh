650b7f310d755632b7e26f69c5ff74140d418a4d
wong73 <120403030+wong73@users.noreply.github.com>
2022-07-08 14:41:54 +0000

    Btrfs: handle EAGAIN case properly in btrfs_drop_snapshot()

--- include/cppdnn/object.hpp
+++ include/cppdnn/object.hpp
@@ -225,4 +225,4 @@ namespace cppdnn
 		virtual std::shared_ptr<basic_object<Ty_>> operator+(const basic_object<Ty_>& object) const override;
 		virtual std::shared_ptr<basic_object<Ty_>> operator*(const basic_object<Ty_>& object) const override;
 		virtual basic_object<Ty_>& operator+=(const basic_object<Ty_>& object) override;
-		virtual basic_object<Ty_>& operator*=(const basi
+		virtual basic_object<Ty_>& operator*=(const basic_object<Ty_>& object) overri
