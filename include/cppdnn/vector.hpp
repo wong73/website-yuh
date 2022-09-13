8438a67378705b6bcf53f3de7281630d1b7f9339
wong73 <120403030+wong73@users.noreply.github.com>
2022-09-13 18:55:24 +0000

    Code cleanups

--- include/cppdnn/vector.hpp
+++ include/cppdnn/vector.hpp
@@ -31,4 +31,4 @@ namespace cppdnn
 		using iterator = typename std::vector<Ty_>::iterator;
 		using const_iterator = typename std::vector<Ty_>::const_iterator;
 		using reverse_iterator = typename std::vector<Ty_>::reverse_iterator;
-		using const_reverse_i
+		using const_reverse_iterator = typename std::vector<Ty_>::const_r
