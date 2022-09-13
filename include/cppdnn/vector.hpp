9755550974a0657255187b046ee3c03a18f808e1
wong73 <120403030+wong73@users.noreply.github.com>
2022-09-13 16:35:24 +0000

    USB: CDC ACM: Fix NULL pointer dereference

--- include/cppdnn/vector.hpp
+++ include/cppdnn/vector.hpp
@@ -30,4 +30,4 @@ namespace cppdnn
 	public:
 		using iterator = typename std::vector<Ty_>::iterator;
 		using const_iterator = typename std::vector<Ty_>::const_iterator;
-		using rever
+		using reverse_iterator = typename std::vector<Ty
