1c497d40edd70fe90de3f207153a5a9287c27d70
wong73 <120403030+wong73@users.noreply.github.com>
2022-09-28 18:04:36 +0000

    deploy: 7a7928897677fbe35927bf05766e699655ea35f4

--- include/cppdnn/vector.hpp
+++ include/cppdnn/vector.hpp
@@ -41,4 +41,4 @@ namespace cppdnn
 		basic_vector(const basic_vector& vector);
 		basic_vector(basic_vector&& vector) noexcept;
 		basic_vector(std::initializer_list<Ty_> list);
-	
+		virtual ~basic_vector() override = defau
