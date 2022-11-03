3019d5e468e3002716490c04895dc47789dea24f
wong73 <120403030+wong73@users.noreply.github.com>
2022-11-03 21:22:16 +0000

    New Design

--- include/cppdnn/vector.hpp
+++ include/cppdnn/vector.hpp
@@ -59,4 +59,4 @@ namespace cppdnn
 		basic_value<Ty_> operator*(const basic_vector<Ty_>& vector) const;
 
 		virtual std::shared_ptr<basic_object<Ty_>> operator+(const basic_object<Ty_>& object) const override;
-		virtual std::shared_ptr<basic_object<Ty_
+		virtual std::shared_ptr<basic_object<Ty_>> operator*(const basic_object<Ty_>& ob
