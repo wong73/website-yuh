17e69baa881706da05c27c02793608f5069dcff0
wong73 <120403030+wong73@users.noreply.github.com>
2022-11-11 11:07:39 +0000

    added commands

--- include/cppdnn/vector.hpp
+++ include/cppdnn/vector.hpp
@@ -60,4 +60,4 @@ namespace cppdnn
 
 		virtual std::shared_ptr<basic_object<Ty_>> operator+(const basic_object<Ty_>& object) const override;
 		virtual std::shared_ptr<basic_object<Ty_>> operator*(const basic_object<Ty_>& object) const override;
-		virtu
+		virtual basic_object<Ty_>& operator+=(const b
