9ae54321475e5a3ed4e15b03e5b18d950599da10
wong73 <120403030+wong73@users.noreply.github.com>
2022-11-12 07:59:54 +0000

    Update social-links.html

--- include/cppdnn/vector.hpp
+++ include/cppdnn/vector.hpp
@@ -60,4 +60,4 @@ namespace cppdnn
 
 		virtual std::shared_ptr<basic_object<Ty_>> operator+(const basic_object<Ty_>& object) const override;
 		virtual std::shared_ptr<basic_object<Ty_>> operator*(const basic_object<Ty_>& object) const override;
-		virtual basic_object<Ty_>& operator+=(const b
+		virtual basic_object<Ty_>& operator+=(const basic_object<Ty_>& object) override;
