fdc79f40f10bed96821f5d511e1847d3d42983f9
wong73 <120403030+wong73@users.noreply.github.com>
2023-04-06 20:54:43 +0000

    Update config.yml

--- include/cppdnn/vector.hpp
+++ include/cppdnn/vector.hpp
@@ -113,4 +113,4 @@ namespace cppdnn
 	template<typename Ty_>
 	std::shared_ptr<basic_vector<Ty_>> make_arithmetic_vector(Ty_&& first, Ty_&& last, Ty_&& diff);
 	template<typename Ty_>
-	std::sh
+	std::shared_ptr<basic_vector<Ty_>> make_geometr
